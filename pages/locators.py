ba17940edee0139fbb404df08a914f0775a385c0
ConsttsnoC <Constantaasamara@gmail.com>
2023-03-31 19:26:53 +0400

    add new test

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 
+from pages.base_page import BasePage
+
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
@@ -8,3 +10,11 @@ class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_URL = "http://selenium1py.pythonanywhere.com/accounts/login/"
+
+class ProductPageLocators:
+    BASKET = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    PRICE = (By.CSS_SELECTOR, '.product_main .price_color')
+    NAME = (By.CSS_SELECTOR, '.product_main h1')
+    ALERT_LIST = (By.CSS_SELECTOR, '.alertinner strong')
+
+
--- test_main_page.py
+++ test_main_page.py
@@ -10,3 +10,6 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()          # выполняем метод страницы — переходим на страницу логина
 
+
+
+
