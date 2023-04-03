f591ce3158d5826d6cdb7af80ebda9ad9166b3b9
ConsttsnoC <Constantaasamara@gmail.com>
2023-04-03 14:55:00 +0400

    add new test

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,11 @@
+import pytest
 from pages.product_page import ProductPage
 
-link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+promo_links = [f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{i}" for i in range(9)]
+promo_links[7] = pytest.param(promo_links[7], marks=pytest.mark.skip(reason="Skipping failing test"))
 
-
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('link', promo_links)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
